c26a8cd997ac9b1ef421e985c2c140cf26817c49
CoolProgrammer2052 <101030561+CoolProgrammer2052@users.noreply.github.com>
2022-03-06 09:06:26 +0530

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Hello, Wishlist Karlson and Enter a Number PLEASE! ";
+    cin >> a;
+    cout << "Cool You Entered some number at least" << endl;
+    cout << a << endl;
+    cout << "Cool Cool" << endl;
+
+    return 0;
+}
